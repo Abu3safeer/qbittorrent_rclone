7ca6bd08b65a05feeb991a85d0131a3b901b79b2
Abu3safeer <abdulazizalmniey@gmail.com>
2020-08-27 04:50:22 +0300

    Init

---
+++ README.md
@@ -0,0 +1,29 @@
+# qBittorent rclone
+
+This script helps you upload files downloaded by qBitorrent to Google Drive using Service accounts.
+
+## How to use?
+* Download [source code folder](https://github.com/Abu3safeer/qbittorrent_rclone/archive/master.zip) or [exe file from releases section](https://github.com/Abu3safeer/qbittorrent_rclone/releases).
+* Copy qbittorrent_rclone folder to your qBitorrent download folder.
+
+![image](https://user-images.githubusercontent.com/12091003/91241948-c9f58e80-e74e-11ea-8b49-4bfd85018c19.png)
+ 
+* folder path will be like this according to the example.
+
+```
+D:\Downloads\Torrent\qbittorrent_rclone
+```
+
+* Now go to Options => download  This check:
+- [x] Run external program on torrent completion
+* write this in the command field:
+```
+python %D/qbittorrent_rclone/[SCRIPT_FILE_NAME] -t "%N" -c "%F" -r "%R" -s "%D"
+```
+* Replace [SCRIPT_FILE_NAME] with py file or exe file you downloaded.
+* put your service accounts json files in accounts folder.
+* put Google Drive folder id in settings.ini file.
+* Select what command used by rclone (Default is move).
+* Write rclone path (System PATH used by default)
+
+You are ready to go.
---
+++ main.py
@@ -0,0 +1,154 @@
+import logging
+import argparse
+import os
+from pathlib import Path
+import configparser
+
+# Get current script path
+script_path = Path(__file__).parent
+
+# Prepare loggin
+log = logging.getLogger('qbittorrent_rclone')
+log.setLevel(1)
+log_format = logging.Formatter('%(asctime)s: %(message)s')
+stream_log = logging.StreamHandler()
+stream_log.setFormatter(log_format)
+file_log = logging.FileHandler(f'{script_path}/log.txt', encoding='utf-8')
+file_log.setFormatter(log_format)
+log.addHandler(stream_log)
+log.addHandler(file_log)
+
+# Prepare arguments receiver
+pars = argparse.ArgumentParser()
+pars.add_argument('--torrent_name', '-t', help='Torrent name')
+pars.add_argument('--content_path', '-c', help='Content path (same as root path for mutilfile torrent)')
+pars.add_argument('--root_path', '-r', help='Root path (first torrent subdirectory path)')
+pars.add_argument('--save_path', '-s', help='Save path')
+pars = pars.parse_args()
+
+if not (pars.torrent_name or pars.content_path or pars.root_path or pars.save_path):
+    log.critical('One argument at least missing, please follow instructions here https://github.com/Abu3safeer/qbittorrent_rclone')
+    exit()
+
+# Prepare directories paths
+accounts_path = Path(f'{script_path}/accounts')
+configs_path = Path(f'{script_path}/configs')
+settings_path = Path(f'{script_path}/settings.ini')
+
+# Check configs directory
+if not configs_path.exists():
+    configs_path.mkdir()
+
+# prepare settings file
+if not (settings_path.is_file() and settings_path.exists()):
+    log.critical('Cannot find file settings.ini, please download it from https://github.com/Abu3safeer/qbittorrent_rclone')
+    exit()
+
+# Prepare settings
+settings = configparser.ConfigParser()
+settings.read(settings_path.resolve())
+
+
+# Check for missing settings
+if not (
+    settings.has_option('main', 'rclone_path') and
+    settings.has_option('main', 'google_drive_folder_id') and
+    settings.has_option('main', 'command') and
+    settings.has_option('internal', 'sa_count') and
+    settings.has_option('internal', 'current_sa')
+):
+    log.critical('Settings.ini file is currupted, please download it new new one from https://github.com/Abu3safeer/qbittorrent_rclone')
+    exit()
+
+# Load settings to variables
+rclone_path = settings.get('main', 'rclone_path')
+
+# Check rclone if uses system PATH or full path
+if rclone_path == 'PATH':
+    rclone_path = 'rclone'
+
+command = settings.get('main', 'command')
+allowed_commands = ['move', 'copy', 'sync']
+if command not in allowed_commands:
+    log.info(f'Cannot find command "{command}" in {allowed_commands}, set to default "move"')
+    command = 'move'
+    settings.set('main', 'command', command)
+
+google_drive_folder_id = settings.get('main', 'google_drive_folder_id')
+if len(google_drive_folder_id.lower()) < 1:
+    log.critical('google_drive_folder_id is empty in setting.ini, cannot proceed')
+    exit()
+
+sa_count = settings.get('internal', 'sa_count')
+current_sa = Path(settings.get('internal', 'current_sa'))
+
+# Check if accounts directory exists
+if not (accounts_path.is_dir() and accounts_path.exists()):
+    accounts_path.mkdir()
+    log.critical('Cannot find accounts folder,put service accounts inside it.')
+    exit()
+
+# Check if accounts folder has service accounts as json
+if not accounts_path.rglob('*.json'):
+    log.critical('accounts folder is empty.')
+    exit()
+
+# Get all service account files
+service_accounts = [account for account in accounts_path.rglob('*.json')]
+
+# Count service accounts
+if len(service_accounts) < 1:
+    log.critical('No service accounts found in accounts folder.')
+    exit()
+
+sa_count = len(service_accounts)
+
+# Get next service account to avoid hitting 750GB limit
+for index, sa in enumerate(service_accounts, 1):
+    if current_sa == sa:
+        if sa_count == index:
+            current_sa = service_accounts[0]
+        else:
+            current_sa = service_accounts[index]
+        break
+
+# If no server account selected from above step, then select first service account found
+if not (current_sa.is_file() and current_sa.exists()):
+    log.info('No service account selected before, setting first one')
+    current_sa = service_accounts[0]
+
+# Update settings
+settings.set('internal', 'sa_count', str(sa_count))
+settings.set('internal', 'current_sa', current_sa.__str__())
+
+# Save settings
+settings_file = open(settings_path, 'w')
+settings.write(settings_file)
+settings_file.close()
+
+# Create config file for selected service account
+config_file_path = Path(f'{configs_path}/{current_sa.name}.conf')
+log.info(f'Config file created: {config_file_path}')
+rclone_config = configparser.ConfigParser()
+rclone_config['qbittorrent_rclone'] = {
+    'type': 'drive',
+    'scope': 'drive',
+    'root_folder_id': google_drive_folder_id,
+    'service_account_file': current_sa.__str__()
+}
+
+# Save config file for selected service account
+config_file = open(config_file_path, 'w')
+rclone_config.write(config_file)
+config_file.close()
+
+log.info(f"Processing file: {pars.torrent_name}")
+
+# Prepare rclone command
+final_command = f'"{rclone_path}" {command} "{pars.content_path}" "qbittorrent_rclone:{pars.torrent_name}" --config "{config_file_path}"'
+
+log.info(f'Run command: {final_command}')
+
+os.system(final_command)
+
+
